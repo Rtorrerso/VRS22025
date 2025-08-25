882cdbed61cc5723021bf54141f345da6c07ba32
Ruben Torres <mr.torres@tes.edu.ec>
2025-08-25 11:35:45 -0500

    update movimiento
    Asignamos movimiento

--- Assets/movimiento.cs
+++ Assets/movimiento.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private Vector3 keyboardRotation = Vector3.zero;
+
     void Start()
     {
-        
+        gyroEnabled = SystemInfo.supportsGyroscope;
+
+        if (gyroEnabled)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gyroEnabled)
+        {
+            transform.localRotation = GyroToUnity(gyro.attitude);
+        }
+        else
+        {
+            float speed = 50f;
+
+            // Ejes horizontales y verticales (WASD y Flechas)
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            keyboardRotation.y += horizontal * speed * Time.deltaTime;
+            keyboardRotation.x -= vertical * speed * Time.deltaTime;
+
+            // Espaciadora para reset
+            if (Input.GetKeyDown(KeyCode.Space))
+                keyboardRotation = Vector3.zero;
+
+            transform.rotation = Quaternion.Euler(keyboardRotation);
+        }
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
     }
 }
+
