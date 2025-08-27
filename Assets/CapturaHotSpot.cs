4e8526d3b37d73d72ca9528f0142b191fcca482e
Ruben Torres <mr.torres@tes.edu.ec>
2025-08-27 15:34:01 -0500

    hotspot-timer y navegacion
    Aqui haremos el hotspot, agregamos timer y generamos la navegacion

--- Assets/CapturaHotSpot.cs
+++ Assets/CapturaHotSpot.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CapturaHotSpot : MonoBehaviour
 {
+    public Vector3 targetPosition = new Vector3(50f, 0f, 0f);
+    public float gazeTime = 3f;
+    private float gazeTimer = 0f;
+    private bool isGazedAt = false;
+
     // Start is called before the first frame update
     LayerMask layerMask;
     void Start()
@@ -25,8 +30,24 @@ public class NewBehaviourScript : MonoBehaviour
             Debug.DrawRay(transform.position, fwd * 5, Color.blue);
             if (hit.collider.gameObject.name == "Patio1")
             {
+                isGazedAt = true;
                 Debug.Log("Vamos al patio1");
+                gazeTimer += Time.deltaTime;
+                if (gazeTimer >= gazeTime)
+                    {
+                    // Mover la cámara principal a la nueva posición
+                    Camera.main.transform.position = targetPosition;
+                    // Reiniciar para evitar múltiples disparos
+                    gazeTimer = 0f;
+                    isGazedAt = false;
+                    }
             }
+            else
+            {
+                gazeTimer = 0f;
+            }
+
+            
         }
         else
         {
--- Assets/rayCast.cs
+++ Assets/rayCast.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CapturaHotSpot : MonoBehaviour
+public class rayCast : MonoBehaviour
 {
     LayerMask layerMask;
 
